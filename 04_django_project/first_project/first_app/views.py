aac93baadd765c9fb583974f33bdcca1f3197596
Brase <brasea@wwt.com>
2022-10-13 21:01:00 -0500

    went over how to create templates and template tags

--- 04_django_project/first_project/first_app/views.py
+++ 04_django_project/first_project/first_app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Test of Htth1Response</h1>')
+    my_dict = {'insert_me':'this is from the views.py file in the app'}
+    return render( request, 'index.html', context= my_dict )
