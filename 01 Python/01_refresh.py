4ce15ea0146d53005adac7b9eb9ddf5a6b000e51
Brase <brasea@wwt.com>
2022-10-16 16:56:55 -0500

    quick python refresher

--- 01 Python/01_refresh.py
+++ 01 Python/01_refresh.py
@@ -1,9 +1,33 @@
-from asyncio import constants
 
+# newlist = ['andrew','steve','dan']
+# secondlist = ['oscar','jake']
+# newlist.extend(secondlist)
+# newlist.append('caleb')
+# print(newlist)
+# ['andrew', 'steve', 'dan', 'oscar', 'jake', 'caleb']
 
-newlist = ['andrew','steve','dan']
-secondlist = ['oscar','jake']
-newlist.extend(secondlist)
-newlist.append('caleb')
-print(newlist)
+def squared(num):
+    return num**2
 
+# map
+numlist = [1,2,3,4,5,6,7,8,9,10]
+results = list(map(squared, numlist))
+print(results)
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# filter
+def list_filter(num):
+    if (num % 2 == 0):
+        return num
+
+filtered = list(filter(list_filter, results))
+print(filtered)
+# [4, 16, 36, 64, 100]
+
+print(filtered.pop())
+# returns the last item of a list (100)
+
+friends = ['andrew','jake','kevin']
+friends.insert(2, 'inserted text')
+print(friends)
+# the text after index 2 will be pushed right ['andrew', 'jake', 'inserted text', 'kevin']
