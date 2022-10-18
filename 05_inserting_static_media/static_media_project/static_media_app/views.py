47ced5687233450d540fcd46c860f7fc1b643cf6
Brase <brasea@wwt.com>
2022-10-17 20:43:00 -0500

    worked on creating views

--- 05_inserting_static_media/static_media_project/static_media_app/views.py
+++ 05_inserting_static_media/static_media_project/static_media_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from static_media_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 def index(request):
-    template_tag = {'temp_tag_one':'this is from the views.py file in the app, this is a template tag'}
-    return render( request, 'static_media_app/static_media_index.html', template_tag)
+
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records':webpages_list}
+
+    # template_tag = {'temp_tag_one':'this is from the views.py file in the app, this is a template tag'}
+    return render( request, 'static_media_app/static_media_index.html', date_dict)
