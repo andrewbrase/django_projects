2130d9634a995e53fc7e8dafc5caa7fc47970fcd
Brase <brasea@wwt.com>
2022-10-19 21:08:23 -0500

    'crud'

--- 06_env/06_django_form/Django_Forms_Project/Django_Forms_app/urls.py
+++ 06_env/06_django_form/Django_Forms_Project/Django_Forms_app/urls.py
@@ -3,4 +3,25 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('add/addrecord/', views.addrecord, name='addrecord'),
+    # So far we have made the user interface, and we point the URL to the view called addrecord, but we have not made the view yet.
+    # Make sure you add the addrecord view in the in the Django_Forms_app/views.py file:
+
+    # The "delete" link in the HTML table points to 127.0.0.1:8000/members/delete/ so we will add a path() 
+    # function in the members/urls.py file, that points the url to the right location, with the ID as a parameter:
+    path('delete/<int:id>', views.delete, name='delete'),
+
+    # Add a path() function in the members/urls.py file, that points the url 127.0.0.1:8000/members/update/ 
+    # to the right location, with the ID as a parameter:
+    path('update/<int:id>', views.update, name='update'),
+
+    # <!-- What Happens on Submit?
+    # Did you notice the action attribute in the HTML form? 
+    # The action attribute specifies where to send the form data, 
+    # in this case the form data will be sent to:
+    # updaterecord/{{ mymember.id }}, so we must add a path() 
+    # function in the members/urls.py file that points to the right view: -->
+    path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord')
+    
 ]
--- 06_env/06_django_form/Django_Forms_Project/Django_Forms_app/views.py
+++ 06_env/06_django_form/Django_Forms_Project/Django_Forms_app/views.py
@@ -1,6 +1,9 @@
+# from json import load
+# from re import template
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Members
 
 # Create your views here.
@@ -19,3 +22,49 @@ def index(request):
     # for member in mymembers:
     #     output += member["firstname"]
     # return HttpResponse(output)
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
+
+# The delete view does the following:
+# Gets the id as an argument.
+# Uses the id to locate the correct record in the Members table.
+# Deletes that record.
+# Redirects the user back to the index view.
+def delete(request, id):
+    member = Members.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+# The update view does the following:
+# Gets the id as an argument.
+# Uses the id to locate the correct record in the Members table.
+# loads a template called update.html.
+# Creates an object containing the member.
+# Sends the object to the template.
+# Outputs the HTML that is rendered by the template.
+
+def update(request, id):
+    mymember = Members.objects.get(id=id)
+    template = loader.get_template('update.html')
+    context = {
+        'mymember_t':mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+    first = request.POST['first']
+    last = request.POST['last']
+    member = Members.objects.get(id=id)
+    member.firstname = first
+    member.lastname = last
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
