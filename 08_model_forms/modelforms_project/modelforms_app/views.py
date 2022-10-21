3b9dab5252a143424e17a6c9757f10fe51b0659b
Brase <brasea@wwt.com>
2022-10-21 12:11:32 -0500

    learning about model forms

---
+++ 08_model_forms/modelforms_project/modelforms_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from . import forms
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def form_name_view(request):
+    form = forms.FormName()
+    return render(request, 'form_page.html', {'form_t' : form})
