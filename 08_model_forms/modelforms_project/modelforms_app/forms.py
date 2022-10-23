9c5a745d91f322c3c42e44a75eeec2501543368e
Brase <brasea@wwt.com>
2022-10-22 20:25:24 -0500

    working with django form POST

--- 08_model_forms/modelforms_project/modelforms_app/forms.py
+++ 08_model_forms/modelforms_project/modelforms_app/forms.py
@@ -4,3 +4,4 @@ class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    # https://docs.djangoproject.com/en/4.1/ref/forms/widgets/#built-in-widgets
--- 08_model_forms/modelforms_project/modelforms_app/views.py
+++ 08_model_forms/modelforms_project/modelforms_app/views.py
@@ -7,6 +7,28 @@ def index(request):
 
 def form_name_view(request):
     form = forms.FormName()
-    return render(request, 'form_page.html', {'form_t' : form})
+    # [22/Oct/2022 20:09:29] "POST /formpage/ HTTP/1.1" 200 1348
+    # after the form data has been posted
 
-    # we need to save the POST data 13:54
+    # this means if someone has hit submit and posted something back
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        # is_valid is a boolean check
+        if form.is_valid():
+            # do something code
+            print('form validation successful')
+            # how to access that data, in the form 
+            # you have the class obj attributes name, email and text, 
+            # thats what you pass in as the key
+            print('NAME: ' + form.cleaned_data['name'])
+            print('EMAIL: ' + form.cleaned_data['email'])
+            print('TEXT: ' + form.cleaned_data['text'])
+            # [22/Oct/2022 20:21:44] "GET /formpage/ HTTP/1.1" 200 1348
+            # form validation successful
+            # Andrew
+            # andrewbrase1@yahoo.com
+            # test
+            # [22/Oct/2022 20:21:49] "POST /formpage/ HTTP/1.1" 200 1398
+
+    return render(request, 'form_page.html', {'form_t' : form})
