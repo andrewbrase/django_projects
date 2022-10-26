b4eda7220dfbbc7f985ae8ab59d47d6e54994022
Brase <brasea@wwt.com>
2022-10-26 08:14:05 -0500

    working on login and signup pages

--- 10_AAA/AAA_project/AAA_app/models.py
+++ 10_AAA/AAA_project/AAA_app/models.py
@@ -1,3 +1,13 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=70, Unique=True)
+    password = models.CharField(max_length=200)
+
+    # ?? You should create a ModelForm (docs), which has a field that uses the PasswordInput widget from the forms library.
+    # you would use a custom validator, specifically MinLengthValidator for min length on password
