549f2ba0602b7893819557c37dbb5261cfb9a735
Brase <brasea@wwt.com>
2022-10-26 09:39:36 -0500

    added in model form

--- 10_AAA/AAA_project/AAA_app/forms.py
+++ 10_AAA/AAA_project/AAA_app/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import Account
 
 class AccountForm(forms.ModelForm):
-    firstname = forms.CharField(label='First Name:', widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
-    lastname = forms.CharField(label='Last Name:', widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
-    email = forms.CharField(label='Email:', widget=forms.TextInput(attrs={'placeholder': 'example@domain.com'}))
-    password = forms.CharField(label='Password:', widget=forms.PasswordInput)
+    firstname = forms.CharField(label='First Name:', widget=forms.TextInput(attrs={'placeholder': 'First Name', 'required': 'true'}))
+    lastname = forms.CharField(label='Last Name:', widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'required': 'true'}))
+    email = forms.CharField(label='Email:', widget=forms.TextInput(attrs={'placeholder': 'example@domain.com', 'required': 'true'}))
+    password = forms.CharField(label='Password:', widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'required': 'true'}))
     class Meta:
         model = Account
         fields = '__all__'
--- 10_AAA/AAA_project/AAA_app/models.py
+++ 10_AAA/AAA_project/AAA_app/models.py
@@ -8,6 +8,3 @@ class Account(models.Model):
     lastname = models.CharField(max_length=50)
     email = models.EmailField(max_length=70)
     password = models.CharField(max_length=200)
-
-    # ?? You should create a ModelForm (docs), which has a field that uses the PasswordInput widget from the forms library.
-    # you would use a custom validator, specifically MinLengthValidator for min length on password
