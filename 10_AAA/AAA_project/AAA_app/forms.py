1d28c325a76c3227295bf4461236b427a8ff8b40
Brase <brasea@wwt.com>
2022-10-26 09:29:03 -0500

    added in model form

--- 10_AAA/AAA_project/AAA_app/forms.py
+++ 10_AAA/AAA_project/AAA_app/forms.py
@@ -2,11 +2,18 @@ from django import forms
 from .models import Account
 
 class AccountForm(forms.ModelForm):
-    # firstname = forms.CharField(max_length=50)
-    # lastname = forms.CharField(max_length=50)
-    # email = forms.EmailField(max_length=70)
-    # password = forms.CharField(widget=forms.PasswordInput)
-
+    firstname = forms.CharField(label='First Name:', widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    lastname = forms.CharField(label='Last Name:', widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    email = forms.CharField(label='Email:', widget=forms.TextInput(attrs={'placeholder': 'example@domain.com'}))
+    password = forms.CharField(label='Password:', widget=forms.PasswordInput)
     class Meta:
         model = Account
         fields = '__all__'
+
+# class UserForm(forms.ModelForm):
+#     first_name = forms.CharField(max_length=50)
+#     last_name = forms.CharField(max_length=50)
+#     email = forms.EmailField(max_length=100)
+#     class Meta:
+#         model = User
+#         fields = '__all__'
