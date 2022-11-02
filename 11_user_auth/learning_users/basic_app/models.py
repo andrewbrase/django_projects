91880bd5358c2f193d03b59093909d78360ba47c
Brase <brasea@wwt.com>
2022-11-01 20:12:47 -0500

    started learning user auth models

--- 11_user_auth/learning_users/basic_app/models.py
+++ 11_user_auth/learning_users/basic_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfileInfo(models.Model):
+
+    # this almost extends the class, 
+    # do not inherit from the User class 
+    # - it does not have multiple instances of this class
+    user = models.OneToOneField(User)
+
+    # additional attributes
+    portfolio_site = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    # > pip install pillow
